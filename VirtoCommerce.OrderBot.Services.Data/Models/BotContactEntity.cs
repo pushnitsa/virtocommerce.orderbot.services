e892bc5dc3ed9a2c006bc25cc081ed3ab54d1bd8
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2019-07-07 22:35:01 +0200

    Removed redundant code

--- VirtoCommerce.OrderBot.Services.Data/Models/BotContactEntity.cs
+++ VirtoCommerce.OrderBot.Services.Data/Models/BotContactEntity.cs
@@ -8,27 +8,6 @@ namespace VirtoCommerce.OrderBot.Services.Data.Models
         [StringLength(128)]
         public string BotUserName { get; set; }
 
-        //public override Member ToModel(Member member)
-        //{
-        //    var result = base.ToModel(member);
-
-        //    var botContact = (BotContact) result;
-        //    botContact.BotUserName = BotUserName;
-
-        //    return result;
-
-        //}
-
-        //public override MemberDataEntity FromModel(Member member, PrimaryKeyResolvingMap pkMap)
-        //{
-        //    base.FromModel(member, pkMap);
-
-        //    var botContact = (BotContact) member;
-        //    BotUserName = botContact.BotUserName;
-
-        //    return this;
-        //}
-
         public override void Patch(MemberDataEntity target)
         {
             base.Patch(target);
